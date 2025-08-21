1899acb5e1cd74ce2cf54be224bea44349cbe5f8
Callum Bondy <callum.bondy@gmail.com>
2025-08-21 13:03:17 -0700

    fixing repsonsive error

--- app/routes/about.jsx
+++ app/routes/about.jsx
@@ -24,10 +24,6 @@ function About() {
   const isMobileActive = useIsMobile(isMobile);
   const location = useLocation();
 
-  // If mobile, render the mobile version
-  if (isMobileActive) {
-    return <AboutMobile staticData={staticData} />;
-  }
 
   // Desktop version
   useEffect(() => {
@@ -50,6 +46,12 @@ function About() {
       return () => clearTimeout(timeout); // Cleanup
     }
   }, [location]);
+
+  // If mobile, render the mobile version
+  if (isMobileActive) {
+    return <AboutMobile staticData={staticData} />;
+  }
+
   return (
     <div>
       <div className="overflow-hidden w-full h-[360px]">
